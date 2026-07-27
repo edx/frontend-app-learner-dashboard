74ceb30fd5e493126a0af8d996687d6cf7d31143
MaxFrank13 <mfrank@2u.com>
2026-07-27 14:31:45 +0000

    feat: add RV dynamic slot to course card action slot

--- src/plugin-slots/CourseCardActionSlot/index.jsx
+++ src/plugin-slots/CourseCardActionSlot/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { PluginSlot } from '@openedx/frontend-plugin-framework';
+import { ErrorBoundary } from '@edx/frontend-platform/react';
+import { UpgradeButton, DynamicExperienceSlot } from '@edx/frontend-plugin-learner-dashboard';
 
 const CourseCardActionSlot = ({ cardId }) => (
   <PluginSlot
@@ -9,7 +11,15 @@ const CourseCardActionSlot = ({ cardId }) => (
     pluginProps={{
       cardId,
     }}
-  />
+  >
+    <ErrorBoundary fallbackComponent={<UpgradeButton cardId={cardId} />}>
+      <DynamicExperienceSlot
+        widgetName="MonarchResolvedDynamicExperience"
+        placement="org.openedx.frontend.learner_dashboard.course_card_action.v1"
+        tenantId="2u-edx-learner-dashboard"
+      />
+    </ErrorBoundary>
+  </PluginSlot>
 );
 
 CourseCardActionSlot.propTypes = {
