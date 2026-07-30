cb0dfefbc2a2a73b9f5f3c6a33cb21a63a8c4b22
mshet <mshet-sonata@2u.com>
2026-07-30 13:26:14 +0000

    feat: test fix

--- src/widgets/LearnerDashboardPlugins/upgrade-button/UpgradeButtonWrapper.test.jsx
+++ src/widgets/LearnerDashboardPlugins/upgrade-button/UpgradeButtonWrapper.test.jsx
@@ -1,35 +1,75 @@
-/* eslint-disable import/no-named-as-default-member */
-import { shallow } from '@edx/react-unit-test-utils';
+import { render, screen } from '@testing-library/react';
+
 import { useCourseData, useEntitlementInfo } from '@src/hooks';
 import UpgradeButtonWrapper from './UpgradeButtonWrapper';
 
 jest.mock('@src/hooks', () => ({
-  useCourseData: jest.fn(() => ({
-    enrollment: { isVerified: false, isExecEd2UCourse: false },
-  })),
-  useEntitlementInfo: jest.fn(() => ({
-    isEntitlement: false,
-  })),
-}), { virtual: true });
+  useCourseData: jest.fn(),
+  useEntitlementInfo: jest.fn(),
+}));
+
+jest.mock('./UpgradeButton', () => ({ cardId }) => (
+  <button data-testid="upgrade-button">
+    Upgrade {cardId}
+  </button>
+));
 
 describe('UpgradeButtonWrapper', () => {
-  const props = {
-    cardId: 'cardId',
-  };
-  describe('snapshot', () => {
-    test('is visible', () => {
-      const wrapper = shallow(<UpgradeButtonWrapper {...props} />);
-      expect(wrapper.snapshot).toMatchSnapshot();
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    useCourseData.mockReturnValue({
+      enrollment: {
+        isVerified: false,
+        isExecEd2UCourse: false,
+      },
     });
-    test('is not visible', () => {
-      useCourseData.mockReturnValue({
-        enrollment: { isVerified: true, isExecEd2UCourse: true },
-      });
-      useEntitlementInfo.mockReturnValue({
-        isEntitlement: true,
-      });
-      const wrapper = shallow(<UpgradeButtonWrapper {...props} />);
-      expect(wrapper.snapshot).toMatchSnapshot();
+
+    useEntitlementInfo.mockReturnValue({
+      isEntitlement: false,
     });
+  });
+
+  it('renders UpgradeButton when course is eligible', () => {
+    render(<UpgradeButtonWrapper cardId="cardId" />);
+
+    expect(screen.getByTestId('upgrade-button')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /upgrade cardid/i })).toBeInTheDocument();
+  });
+
+  it('does not render UpgradeButton when the course is verified', () => {
+    useCourseData.mockReturnValue({
+      enrollment: {
+        isVerified: true,
+        isExecEd2UCourse: false,
+      },
+    });
+
+    render(<UpgradeButtonWrapper cardId="cardId" />);
+
+    expect(screen.queryByTestId('upgrade-button')).not.toBeInTheDocument();
+  });
+
+  it('does not render UpgradeButton when the course is an entitlement', () => {
+    useEntitlementInfo.mockReturnValue({
+      isEntitlement: true,
+    });
+
+    render(<UpgradeButtonWrapper cardId="cardId" />);
+
+    expect(screen.queryByTestId('upgrade-button')).not.toBeInTheDocument();
+  });
+
+  it('does not render UpgradeButton when the course is an ExecEd course', () => {
+    useCourseData.mockReturnValue({
+      enrollment: {
+        isVerified: false,
+        isExecEd2UCourse: true,
+      },
+    });
+
+    render(<UpgradeButtonWrapper cardId="cardId" />);
+
+    expect(screen.queryByTestId('upgrade-button')).not.toBeInTheDocument();
   });
 });
